8fec88a1930d12cc640edf4259cb8470a0278679
Vladimir Scherbina <vns.scherbina@gmail.com>
2021-10-25 22:00:41 +0300

    Initial commit

---
+++ myfirst/apps/articles/models.py
@@ -0,0 +1,36 @@
+"""
+Data models
+"""
+import datetime
+from django.db import models
+
+from django.utils import timezone
+
+
+class Article(models.Model):
+    article_title = models.CharField('article title', max_length=200)
+    article_text = models.TextField('article text')
+    pub_date = models.DateTimeField('publication date')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now()-datetime.timedelta(days=7))
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField('author name', max_length=50)
+    comment_text = models.TextField('comment text')
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Коментарий'
+        verbose_name_plural = 'Комментарии'
